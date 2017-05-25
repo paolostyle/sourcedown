a3ed7e0993f10830626e01a970abda55d6f5b3b4
paolostyle <tuptus95@gmail.com>
2017-05-25 23:02:00 +0200

    "final" version

--- app/src/main/java/org/practice/pawel/sourcedown/MainFragment.java
+++ app/src/main/java/org/practice/pawel/sourcedown/MainFragment.java
@@ -1,10 +1,12 @@
 package org.practice.pawel.sourcedown;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.AsyncTask;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,21 +15,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.ConnectException;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import okio.Buffer;
-import okio.BufferedSink;
-import okio.Okio;
-import okio.Source;
 
 public class MainFragment extends Fragment {
     public MainFragment() {}
@@ -35,10 +28,28 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        final Button clickButton = (Button) rootView.findViewById(R.id.download);
         final EditText urlAddress = (EditText) rootView.findViewById(R.id.urlAddress);
+
+        // "Done" on soft keyboard triggers Button click
+        urlAddress.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    switch (keyCode) {
+                        case KeyEvent.KEYCODE_DPAD_CENTER:
+                        case KeyEvent.KEYCODE_ENTER:
+                            clickButton.performClick();
+                            return true;
+                        default:
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
+        //editing starts after http://
         urlAddress.setSelection(urlAddress.getText().length());
 
-        Button clickButton = (Button) rootView.findViewById(R.id.download);
         clickButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,6 +70,13 @@ public class MainFragment extends Fragment {
         private final OkHttpClient client = new OkHttpClient();
         private boolean errorOccurred = false;
         private DBHandler db = new DBHandler(getContext());
+        private ProgressDialog progress = new ProgressDialog(getContext());
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(getContext(), getString(R.string.downloading),
+                    getString(R.string.downloading_long), true);
+        }
 
         @Override
         protected String doInBackground(String...params) {
@@ -92,18 +110,22 @@ public class MainFragment extends Fragment {
                 return e.getMessage();
             }
         }
-
         @Override
         protected void onPostExecute(String result) {
             if(errorOccurred) {
+                progress.dismiss();
                 Helper.displayAlert(getActivity(), getString(R.string.alert_error), result);
             }
             else {
+                //hides softkeyboard if successfully source has been succesfully downloaded
                 InputMethodManager imm = (InputMethodManager)(getContext().getSystemService(Context.INPUT_METHOD_SERVICE));
                 imm.hideSoftInputFromWindow(getView().getRootView().getWindowToken(), 0);
+
                 TextView textView = (TextView) getActivity().findViewById(R.id.content_view);
                 textView.setText(result);
+                progress.dismiss();
             }
+
         }
     }
 }
